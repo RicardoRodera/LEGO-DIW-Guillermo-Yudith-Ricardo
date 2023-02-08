3d46f4c28e3f2275f1fedc91f32faa05083b1f82
Guillermo Suárez Dosantos <guillermosd92@gmail.com>
2023-02-08 14:16:27 +0100

    8/2/2023

--- js/pruebasGET.js
+++ js/pruebasGET.js
@@ -1,7 +1,7 @@
 const key = "07880df945ae318d79416922e15e7c11"
 
 
-function getSetsBusqueda(busqueda) {
+function getSetsBusqueda(busqueda="") {
     fetch("https://rebrickable.com/api/v3/lego/sets/?search=" + busqueda + "&key=" + key, { method: 'get' })
         .then(function (respuesta) {
             return respuesta.json()
@@ -13,3 +13,4 @@ function getSetsBusqueda(busqueda) {
             console.error('Error', ex.message)
         })
 }
+getSetsBusqueda();
