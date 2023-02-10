e37e0c0d8d6511647a293493d0b86b174a001906
Yudith Lorenzo González <yudithlorenzog@hotmail.com>
2023-02-10 09:58:43 +0100

    Unificación repositorios

--- js/menuFiguras.js
+++ js/menuFiguras.js
@@ -1,37 +1,61 @@
-window.addEventListener("load", iniciar);
+window.addEventListener("load", cargarPagina);
 
 const key = "07880df945ae318d79416922e15e7c11";
+let colores = ["rojo", "azul", "verde", "amarillo"];
+let color=0;
 
-function iniciar(){
-   
-  fetch("https://rebrickable.com/api/v3/lego/minifigs/?key="+key)
+var tamPagina=16;
+var paginaActual=1;
+var totalFiguras = 0;
+
+function cargarPagina(){
+  mostrarApi();
+  this.document.querySelector("#anterior").addEventListener("click",pulsaAnterior);
+  this.document.querySelector("#siguiente").addEventListener("click",pulsaSiguiente);
+}
+
+
+function mostrarApi(){
+  //https://rebrickable.com/api/v3/lego/minifigs/?key=${key}&limit=${tamPagina}&offset=${(paginaActual-1)*tamPagina}
+  fetch(`https://rebrickable.com/api/v3/lego/minifigs/?key=${key}&limit=${tamPagina}&offset=${(paginaActual-1)*tamPagina}`)
       .then(response => response.json())
       .then(data => {
-
+        totalFiguras = data.count;
+        var figuras = data.results;
+        console.log(figuras);
       data.results.forEach(figuras => {
-        
+       
         let tarjeta = `
           <div class="col-lg-3 col-md-6 col-sm-12 d-flex justify-content-center pb-5 pt-5">
-            <div class="card" style="width: 18em;">
+            <div class="card ${colores[color]} border border-light rounded" style="width: 18em;">
               <div class="bg-light contenedorImagen">
                 ${comprobarImagen(figuras.set_img_url)}
               </div>
-              <div class="card-body mt-3">
-                <h5 class="card-title ">${figuras.name}</h5>
-                <p class="card-text ">Numero de partes: ${figuras.num_parts}</p>
-                <p class="card-text ">ID#${figuras.set_num}</p>
+              <div class="card-body mt-3 ">
+                <h5 class="card-title text-light">${figuras.name}</h5>
+                <p class="card-text text-light">Numero de partes: ${figuras.num_parts}</p>
+                <p class="card-text text-light">ID#${figuras.set_num}</p>
               </div>
             </div>
           </div>`;
 
         document.getElementById('catalogo').innerHTML += tarjeta;
+
+        color++;
+        if(color==4){
+          color=0
+        };
+        
+       
     });
+    actualizaPaginacion(data);
    
   }).catch(function (ex) {
     console.error('Error', ex.message)
   })
-
+  color=0;
 }
+
 
 function getSetsBusqueda(busqueda="") {
   fetch("https://rebrickable.com/api/v3/lego/sets/?search=" + busqueda + "&key=" + key, { method: 'get' })
@@ -53,6 +77,39 @@ function comprobarImagen(valor){
     return  `<img src="../Imagenes/LegoVacio.png" class="card-img-top">`;
   }
 }
+
+function actualizaPaginacion(data){
+  document.querySelector("#numeroPagina").innerHTML = paginaActual;
+  document.querySelector("#totalPaginas").innerHTML = Math.ceil(data.count/tamPagina);
+
+  if(paginaActual==1)
+    document.querySelector("#anterior").classList.add("disabled");
+  else{
+    if(paginaActual==Math.ceil(data.count/tamPagina)){
+      document.querySelector("#siguiente").classList.add("disabled");
+    }else{
+      document.querySelector("#anterior").classList.remove("disabled");
+      document.querySelector("#siguiente").classList.remove("disabled");
+    }
+  }
+}
+
+function cargaResultados(){
+  document.getElementById("catalogo").innerHTML = "";
+  mostrarApi();
+}
+
+
+function pulsaAnterior(){
+  paginaActual--;
+  cargaResultados();
+ 
+ }
+ 
+ function pulsaSiguiente(){
+   paginaActual++;
+   cargaResultados();
+ }
 
 //getSetsBusqueda();
 
---
+++ js/menuSets.js
@@ -0,0 +1,61 @@
+window.addEventListener("load", init);
+const key = "07880df945ae318d79416922e15e7c11"
+const temas = new Map();
+
+function init() {
+    //document.getElementById("btnBuscar").addEventListener("click", buscar);
+    //document.getElementById("tema").addEventListener("input", autocompletar);
+    getTemas();
+    console.log(temas);
+    
+
+}
+
+//Los temas estan en un map, que tiene en cada posicion el nombre y el id de cada tema.
+function getTemas() {
+    let listaTemas = [];
+    fetch("https://rebrickable.com/api/v3/lego/themes/?page_size=460&key=" + key, { method: 'get' })
+        .then(function (respuesta) {
+            return respuesta.json()
+        })
+        .then(function (jsonData) {
+            let results = jsonData.results;
+            for (let i = 0; i < results.length; i++) {
+                temas.set(results[i].id, results[i].name);
+            }
+        })
+        .catch(function (ex) {
+            console.error('Error', ex.message)
+        })
+}
+
+function buscar() {
+    //let busqueda = document.querySelector("#busqueda").value;
+    //let anio = document.querySelector("#anio").value;
+    //let piezas = document.querySelector("#piezas").value;
+    //let tema = document.querySelector("#tema").value;
+
+    fetch("https://rebrickable.com/api/v3/lego/sets/?search=" + busqueda + "&page_size=99999&theme_id=" + tema + "&min_year=" + anio + "&max_year=" + anio +  "&key=" + key, { method: 'get' })
+        .then(function (respuesta) {
+            return respuesta.json()
+        })
+        .then(function (jsonData) {
+            console.log(jsonData)
+            for (let i = 0; i < jsonData.results.length; i++) {
+                setJson = jsonData.results[i];
+                console.log("Nombre del set: " + setJson.name);
+                console.log("Año de salida del set: " + setJson.year);
+                console.log("Imagen del set: " + setJson.set_img_url);
+            }
+        })
+        .catch(function (ex) {
+            console.error('Error', ex.message)
+        })
+}
+
+function autocompletar() {
+
+}
+
+
+
