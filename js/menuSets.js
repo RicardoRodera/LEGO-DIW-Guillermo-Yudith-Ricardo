94103df554b279a56b78546b115891d9bcfbe9f3
Ricardo Rodera Álvarez <jew16373@educastur.es>
2023-02-15 13:37:41 +0100

    El formulario de Sets se borra tras hacer una busqueda

--- js/menuSets.js
+++ js/menuSets.js
@@ -1,16 +1,18 @@
 window.addEventListener("load", init);
 const key = "07880df945ae318d79416922e15e7c11"
+//Creo un mapa en el que guardaré los temas de los distintos sets para mostrar las sugerencias
 const temas = new Map();
 
+//Lleno el mapa y añado los listeners
 function init() {
     document.getElementById("btnBuscar").addEventListener("click", buscar);
     document.getElementById("buscarTemas").addEventListener("input", autocompletar);
     getTemas();
-    console.log(temas);
 
 }
 
 //Los temas estan en un map, que tiene en cada posicion el nombre y el id de cada tema.
+//Aqui se hace la llamada a la API que los obtiene
 function getTemas() {
     let listaTemas = [];
     fetch("https://rebrickable.com/api/v3/lego/themes/?page_size=460&key=" + key, { method: 'get' })
@@ -28,12 +30,14 @@ function getTemas() {
         })
 }
 
+
+//Esta funcion es la que recibe los datos del formulario y hace la llamada a la API en consonancia
 function buscar() {
     let busqueda = document.querySelector("#buscarSets").value;
     let anio = document.querySelector("#buscarAño").value;
     let piezas = document.querySelector("#buscarPiezas").value;
     console.log(piezas);
-    let tema = document.querySelector("#buscarTemas").value ? temas.get(document.querySelector("#buscarTemas").value) : "";
+    let tema = document.querySelector("#buscarTemas").value != "" ? temas.get(document.querySelector("#buscarTemas").value) : "";
 
     fetch("https://rebrickable.com/api/v3/lego/sets/?search=" + busqueda + "&page_size=99999&theme_id=" + tema + "&min_year=" + anio + "&max_year=" + anio + "&min_parts=" + piezas + "&max_parts=" + piezas + "&key=" + key, { method: 'get' })
         .then(function (respuesta) {
@@ -51,7 +55,13 @@ function buscar() {
         .catch(function (ex) {
             console.error('Error', ex.message)
         })
+
+    document.querySelector("#buscarSets").value = "";
+    document.querySelector("#buscarAño").value = "";
+    document.querySelector("#buscarPiezas").value = "";
+    document.querySelector("#buscarTemas").value = "";
 }
+
 
 function autocompletar(e) {
     cierraSugerencias();
@@ -67,7 +77,7 @@ function autocompletar(e) {
 
     this.parentNode.appendChild(lista);
     temas.forEach((value, key) => {
-        /* Crea un option para cada pais que comienza igual que el texto que he introducido */
+        /* Crea un option para cada tema que comienza igual que el texto que he introducido */
         if (key.toLowerCase().startsWith(valor.toLowerCase())) {
             let sugerencia = document.createElement("option");
             sugerencia.id = value;
